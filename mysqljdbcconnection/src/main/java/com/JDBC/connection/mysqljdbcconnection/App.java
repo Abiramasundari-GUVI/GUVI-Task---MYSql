c7d169cf0eb6d0add3dce9258c73b6d7ca9f46c0
Abiramasundari-GUVI <pasupathinathan2018@gmail.com>
2024-12-15 23:27:28 +0530

    Guvi task - MYSQL

---
+++ mysqljdbcconnection/src/main/java/com/JDBC/connection/mysqljdbcconnection/App.java
@@ -0,0 +1,10 @@
+package com.JDBC.connection.mysqljdbcconnection;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
